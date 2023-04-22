414e7e873eb2380f33ae8d5690a987046667d18e
linxin1019 <linxin1019@hotmail.com>
2023-04-22 23:58:51 +0900

    feat: enable cache

--- alj-java-challenge-master/src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
+++ alj-java-challenge-master/src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
@@ -3,6 +3,8 @@ package jp.co.axa.apidemo.services;
 import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,6 +21,7 @@ public class EmployeeServiceImpl implements EmployeeService{
         return employees;
     }
 
+    @Cacheable(value = "employee", key = "#employee.id")
     public Employee getEmployee(Long employeeId) {
         Optional<Employee> optEmp = employeeRepository.findById(employeeId);
         return optEmp.orElse(null);
@@ -28,10 +31,12 @@ public class EmployeeServiceImpl implements EmployeeService{
         employeeRepository.save(employee);
     }
 
+    @CacheEvict(value = "employee", key = "#employeeId")
     public void deleteEmployee(Long employeeId){
         employeeRepository.deleteById(employeeId);
     }
 
+    @Cacheable(value = "employee", key = "#employee.id")
     public void updateEmployee(Employee employee) {
         employeeRepository.save(employee);
     }
